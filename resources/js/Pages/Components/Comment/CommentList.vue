73ecce2c5d218a83a04eeff706ed1ee733943bc8
Dewantara675 <rumiachan35@gmail.com>
2025-10-02 17:24:21 +0700

    pagination comment

--- resources/js/Pages/Components/Comment/CommentList.vue
+++ resources/js/Pages/Components/Comment/CommentList.vue
@@ -24,6 +24,18 @@ function goTo(url) {
       <div v-for="comment in comments.data" :key="comment.id" class="border-t pt-4 mt-4">
             <CommentItem :comment="comment"/>
       </div>
+      <!-- Pagination -->
+    <div class="flex space-x-2 mt-4">
+      <button
+        v-for="link in comments.links"
+        :key="link.label"
+        @click="goTo(link.url)"
+        v-html="link.label"
+        class="px-3 py-1 border rounded"
+        :class="{ 'bg-blue-500 text-white': link.active, 'opacity-50': !link.url }"
+        :disabled="!link.url"
+      />
+    </div>
     </div>
       <div v-else>Tidak ada komentar</div>
     </div>
